77904e1fd1bbba13e1059cf4dca4caf585988885
Jim Kang <jimkang@gmail.com>
2023-08-09 23:00:57 -0400

    Update script to go through all source svgs and produce various size pngs for each.

--- meta/snapshots/svgs-to-pngs.sh
+++ meta/snapshots/svgs-to-pngs.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# inkscape -w 9300 -h 7750 svg/Muse-Hills.svg -o big-png/Muse-Hills.png
-# TODO: Make all the sizes for each svg.
+for file in svg/*.svg
+  do
+    filename="${file##*/}"
+    basename="${filename%.*}"
+    inkscape -w 9300 -h 7750 svg/${basename}.svg -o big-png/${basename}.png
+    inkscape -w 6600 -h 9900 svg/${basename}.svg -o big-png/${basename}-2x3.png
+    inkscape -w 14579 -h 5700 svg/${basename}.svg -o big-png/${basename}-tapestry.png
+    inkscape -w 3750 -h 10650 svg/${basename}.svg -o big-png/${basename}-tall.png
+  done
